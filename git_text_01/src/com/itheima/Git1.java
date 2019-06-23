9d46390b3177e264093440998b9ba81719fe5773
lisi <lisi@itcast.cn>
2019-06-23 14:45:39 +0800

    修改git1文件

--- git_text_01/src/com/itheima/Git1.java
+++ git_text_01/src/com/itheima/Git1.java
@@ -7,5 +7,7 @@ public class Git1 {
         System.out.println("111111");
         System.out.println("222222");
         System.out.println("333333");
+        System.out.println("444444");
+        System.out.println("555555");
     }
 }
