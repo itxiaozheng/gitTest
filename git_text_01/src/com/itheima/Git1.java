51baa16942872043098242407f6d85079db56e03
lisi <lisi@itcast.cn>
2019-06-23 14:41:50 +0800

    添加git1文件

---
+++ git_text_01/src/com/itheima/Git1.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class Git1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("111111");
+        System.out.println("222222");
+        System.out.println("333333");
+    }
+}
